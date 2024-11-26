0f50c11b7d9456b76c762ae278082041ae992701
irene cascon <casconrosado@gmail.com>
2024-11-26 08:35:33 +0100

    Ejercicio6

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -6,7 +6,7 @@ import random
 # Crear el evento que representa la señal de salida
 salida_evento = threading.Event()
 
-# Función que simula el comportamiento de cada corredor
+
 def corredor(id_corredor):
     print(f"Corredor {id_corredor} en posición, esperando señal de salida...")
     salida_evento.wait()  # Esperar hasta que se active el evento
@@ -15,24 +15,20 @@ def corredor(id_corredor):
     time.sleep(tiempo_carrera)  # Simular el tiempo de carrera
     print(f"Corredor {id_corredor} ha llegado a la meta.")
 
-# Función para iniciar la carrera
 def iniciar_carrera():
     print("Señal de salida en 2 segundos...")
     time.sleep(2)  # Pausa de 2 segundos antes de dar la salida
     salida_evento.set()  # Activar el evento
     print("¡Salida! Los corredores han comenzado.")
 
-# Crear e iniciar los hilos de los corredores
 hilos_corredores = []
 for i in range(5):  # 5 corredores
     hilo = threading.Thread(target=corredor, args=(i,))
     hilos_corredores.append(hilo)
     hilo.start()
 
-# Iniciar la carrera
 iniciar_carrera()
 
-# Esperar a que todos los corredores terminen la carrera
 for hilo in hilos_corredores:
     hilo.join()
 
